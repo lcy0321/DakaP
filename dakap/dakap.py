7438e9cb2cec40666ec32c2d366df2018be6abd1
LCY <u900011@gmail.com>
2021-09-12 13:09:51 +0800

    Remove slash commands

--- dakap/dakap.py
+++ dakap/dakap.py
@@ -12,9 +12,6 @@ from typing import Optional, TypeVar
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
-from discord_slash import SlashCommand, SlashContext
-
-from dakap.common import SendFunc
 
 from .count_emojis import count_emojis
 from .get_time import get_time
@@ -25,23 +22,6 @@ logger.setLevel(logging.DEBUG)
 logging.basicConfig(format='%(asctime)s:%(levelname)-7s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 T = TypeVar('T')
-
-
-class InteractionResponseType(IntEnum):
-    """InteractionResponseType for Slash Command Interaction Response
-    Refers to:
-    https://discord.com/developers/docs/interactions/slash-commands#interaction-response
-    """
-    # ACK a `Ping`
-    Pong = 1
-    # ACK a command without sending a message, eating the user's input
-    Acknowledge = 2
-    # respond with a message, eating the user's input
-    ChannelMessage = 3
-    # respond with a message, showing the user's input
-    ChannelMessageWithSource = 4
-    # ACK a command without sending a message, showing the user's input
-    AcknowledgeWithSource = 5
 
 
 class DakaPBot(Bot):
@@ -73,21 +53,6 @@ bot = DakaPBot(
     case_insensitive=True,
     description='Source: https://github.com/lcy0321/DakaP',
 )
-slash = SlashCommand(bot, auto_register=True)
-
-
-async def _ack_slash_tirggered(ctx: SlashContext):
-    await ctx.send(send_type=InteractionResponseType.AcknowledgeWithSource)
-
-
-def _wrap_slash_context_send(ctx: SlashContext, hidden: bool = False) -> SendFunc:
-    async def send(content: str):
-        await ctx.send(
-            send_type=InteractionResponseType.ChannelMessage,
-            content=content,
-            hidden=hidden,
-        )
-    return send
 
 
 @bot.command()
@@ -127,20 +92,6 @@ async def time(ctx: Context, *time_args: str):
 async def clean(ctx: Context, limit: Optional[int]):
     """Search for specific number of messages, and delete those sent by this bot."""
     return await clean_my_messages(channel=ctx.channel, limit=limit)
-
-
-@slash.slash(name='emoji', description='Count the emojis in the guild.')
-async def slash_emoji(ctx: SlashContext):
-    """Count the emojis in the guild."""
-    await _ack_slash_tirggered(ctx=ctx)
-    async with ctx.channel.typing():
-        return await count_emojis(send=_wrap_slash_context_send(ctx=ctx), guild=ctx.guild)
-
-
-@slash.slash(name='time', description='Get time in different time zones.')
-async def slash_time(ctx: SlashContext, *time_args: str):
-    """Get time in different time zones."""
-    return await get_time(send=_wrap_slash_context_send(ctx=ctx, hidden=True), time_args=time_args)
 
 
 def main():
